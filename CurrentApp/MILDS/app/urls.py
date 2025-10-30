f4da6c1dac8e06b67617f61abd6ec41cf5c242e8
Medapati <sathvik.medapati@westpoint.edu>
2025-10-30 11:31:28 -0400

    Add Django Ninja configuration and sample /add endpoint

--- CurrentApp/MILDS/app/urls.py
+++ CurrentApp/MILDS/app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include   # <-- include!
+from .api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.back_end.urls")),  # <-- add this line
+    path("api/", api.urls),
 ]
